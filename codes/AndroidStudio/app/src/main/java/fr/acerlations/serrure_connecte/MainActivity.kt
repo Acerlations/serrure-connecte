3a6faf25d71245642fb008918c24dedbc099a39b
Alexandre <46300952+Acerlations@users.noreply.github.com>
2023-03-27 12:15:48 +0200

    Android Studio init

---
+++ codes/AndroidStudio/app/src/main/java/fr/acerlations/serrure_connecte/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.acerlations.serrure_connecte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
